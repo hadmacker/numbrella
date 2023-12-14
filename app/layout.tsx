2edf9675e542796da9aa0cb7f9a1c7448fd922a5
Tyler Dueck <hadmacker@gmail.com>
2023-12-13 18:09:31 -0600

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Numbrillig',
-  description: 'Hyperlexic and hypernumeric kids can find an escape in numbers and words.',
+  description: 'Finding an escape in numbers and words.',
 }
 
 export default function RootLayout({
