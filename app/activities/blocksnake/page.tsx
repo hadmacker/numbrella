99ad8c0627798db1de19edb5a3b3195514eaee52
hadmacker <hadmacker@gmail.com>
2024-03-02 10:31:03 -0600

    number snake

---
+++ app/activities/blocksnake/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React, { useEffect, useState } from "react";
+
+const Game: React.FC = () => {
+
+  return (
+    <div>
+      <h1>Number Snake</h1>
+      <p>Coming soon...</p>
+    </div>
+  )
+};
+
+export default Game;
