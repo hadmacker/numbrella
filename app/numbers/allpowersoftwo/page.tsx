3e64ab5cdf403be7e4bbf907dcdd5337fd6bddb1
hadmacker <hadmacker@gmail.com>
2024-09-01 09:08:08 -0500

    more powers

---
+++ app/numbers/allpowersoftwo/page.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import React from "react";
+import { PrettyChar } from '../../prettyChar'
+import { NumberFormatter } from '../../numberFormatter'
+
+const numbers = PrettyChar.allCharacters();
+
+function formatted(number: number, bw: boolean): React.ReactElement[] {
+  const digitsArray = NumberFormatter.formatNumberWithBreaks(number.toString()).split("");
+
+  // Create an array of <div> elements to hold each digit
+  const divElements: React.ReactElement[] = digitsArray.map((digit, index) => {
+    const prettyStyle = (bw.toString() == "true")
+      ? numbers.filter(n=> n.id == "0")[0].prettyStyle
+      : numbers.filter(n=> n.id == digit)[0].prettyStyle;
+    const combinedClasses = `${prettyStyle}`;
+    return (
+      <span className={combinedClasses} key={index}>
+        {digit}
+      </span>
+    );
+  });
+
+  return divElements;
+}
+
+export default function Page() {
+  const n = 55;
+  const divElements: React.ReactElement[] = [];
+
+  for (let i = 0; i <= n; i++) {
+    const answer = Math.pow(2, i);
+    divElements.push(
+      <div key={i}>
+        {Formula(i, 2, answer, false)}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <span className="mx-3 md:mx-2 md:p-1 text-2xl md:text-3xl font-black text-gray-500">List of Powers of Two</span>
+      <div className="columns-1 text-left">
+        {divElements}
+      </div>
+    </>
+  )
+}
+
+function Formula(power: number, base: number, answer: number, bw: boolean) {
+  return (
+    <>
+      <p className="text-left m-5 tracking-wide font-mono text-5xl font-black">
+        {formatted(base, bw)}
+        <sup>{formatted(power, bw)}</sup>
+        <span className="px-4">=</span>
+        {formatted(answer, bw)}
+      </p>
+    </>
+    )
+}
